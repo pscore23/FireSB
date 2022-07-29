0a2c47c459613646aa38d7b08c7edc14af745b16
pscore23 <rubik2357x@gmail.com>
2022-07-29 22:59:35 +0900

    "actual_names" 変数と関連する "files" 変数を追加

--- _unzipper.py
+++ _unzipper.py
@@ -9,13 +9,23 @@ class SB3:
     def open(cls, file_name):
         need = "project.json"
 
-        with zipfile.ZipFile(file_name) as zf:
-            names = zf.namelist()
+        try:
+            with zipfile.ZipFile(file_name) as zf:
+                names = zf.namelist()
 
-            if need not in names:
-                raise _except.JSONNotFoundError("'.sb3' file must contain 'project.json' file")
+                if need not in names:
+                    raise _except.JSONNotFoundError("'.sb3' file must contain 'project.json' file")
 
-            json_file = json.dumps(json.loads(
-                zf.read(need).decode("utf-8")), ensure_ascii=False, indent=4, sort_keys=False, separators=(',', ': '))
+                json_file = json.dumps(json.loads(
+                    zf.read(need).decode("utf-8")), ensure_ascii=False, indent=4, sort_keys=False, separators=(',', ': '))
+                actual_names = (item for item in names if (
+                    item.count(".") == 1 and
+                    len(item.split(".")[0]) == 32 and
+                    item.split(".")[0].isalnum()
+                ))
+                files = tuple(zf.open(name) for name in actual_names)
 
-        return json_file
+            return json_file, files
+
+        except zipfile.BadZipFile:
+            raise _except.NotSB3Error("File with extension '.sb3' is not specified.")

894c290a7c9f6aeb02571370370d9e757aa1e3fa
pscore23 <rubik2357x@gmail.com>
2022-08-03 16:25:45 +0900

    "MainWindow" クラスの仕様変更

--- gui.py
+++ gui.py
@@ -1,27 +1,31 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import filedialog, ttk
 
 
-class MainWindow:
+class MainWindow(tk.Frame):
     def __init__(self, width: int, height: int, title: str) -> None:
         self.root = tk.Tk()
-        self.root.title(title)
+        super().__init__(self.root)
 
         sw, sh = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
         w, h = width, height
 
+        self.root.title(title)
         self.root.geometry(f"{w}x{h}+{int(sw/2-w/2)}+{int(sh/2-h/2)}")
 
-    def set_label(self) -> None:
-        label = ttk.Label(self.root, text="Test")
-        label.pack()
+        self.frame = tk.Frame(self.root)
+        self.frame.pack()
+
+    def run(self) -> None:
+        self._update()
+        self.root.mainloop()
 
     def _update(self) -> None:
         self.root.update_idletasks()
 
-    def start(self) -> None:
-        self._update()
-        self.root.mainloop()
-
     def _close(self) -> None:
         self.root.destroy()
+
+    def set_label(self, text: str) -> None:
+        label = ttk.Label(self.frame, text=text)
+        label.pack()

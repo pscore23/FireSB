45c5a5743cf2c5bed38144f30fd1b63dab634c86
pscore23 <rubik2357x@gmail.com>
2022-08-04 19:26:24 +0900

    .sb3 ファイルパス取得までの GUI の大部分の完成

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import filedialog, ttk
 
@@ -6,6 +7,9 @@ class MainWindow(tk.Frame):
     def __init__(self, width: int, height: int, title: str) -> None:
         self.root = tk.Tk()
         super().__init__(self.root)
+
+        self.file_name = tk.StringVar()
+        self.file_name.set("未選択")
 
         sw, sh = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
         w, h = width, height
@@ -16,6 +20,11 @@ class MainWindow(tk.Frame):
         self.frame = tk.Frame(self.root)
         self.frame.pack()
 
+        self.set_button("参照...", 40, self.open_file)
+        self.set_label(0, self.file_name)
+
+        self.run()
+
     def run(self) -> None:
         self._update()
         self.root.mainloop()
@@ -23,9 +32,22 @@ class MainWindow(tk.Frame):
     def _update(self) -> None:
         self.root.update_idletasks()
 
-    def _close(self) -> None:
+    def close(self) -> None:
         self.root.destroy()
 
-    def set_label(self, text: str) -> None:
-        label = ttk.Label(self.frame, text=text)
-        label.pack()
+    def open_file(self, event) -> None:
+        f_type = [("Scratch プロジェクトファイル", "*.sb3")]
+        i_dir = os.path.abspath(os.path.dirname(__file__))
+        file_name = filedialog.askopenfilename(filetypes=f_type, initialdir=i_dir)
+
+        if not len(file_name) == 0:
+            self.file_name.set(file_name)
+
+    def set_label(self, pad_y: int, text_var) -> None:
+        label = ttk.Label(self.frame, textvariable=text_var)
+        label.pack(pady=pad_y)
+
+    def set_button(self, text: str, pad_y: int, handler) -> None:
+        button = ttk.Button(self.frame, text=text)
+        button.bind("<ButtonPress>", handler)
+        button.pack(pady=pad_y)

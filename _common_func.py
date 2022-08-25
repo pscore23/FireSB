f0b3dd08bba3bd6c21a74a79d2f77813a2032c1f
pscore23 <rubik2357x@gmail.com>
2022-08-25 22:00:25 +0900

    "generate_random_string" 関数を追加

---
+++ _common_func.py
@@ -0,0 +1,6 @@
+import random
+import string
+
+
+def generate_random_string(nos, los):
+    return ["".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for j in range(los)) for i in range(nos)]

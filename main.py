f400f1a01c3189265337802c3f5ff18caf158875
pscore23 <rubik2357x@gmail.com>
2022-08-27 16:56:17 +0900

    細かな変更

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import time
 
 CD_PATH = os.getcwd()
 
-print("セットアップしています...")
-os.system(rf"{CD_PATH}\setup.bat")
-print("セットアップが完了しました!")
-
-time.sleep(0.3)
-
 print("プログラムを実行します...")
 os.system(rf"python -O {CD_PATH}\runtime.py")
+
+time.sleep(0.3)

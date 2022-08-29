12a2047cefebfe205d180a9384fa7ead32749074
pscore23 <rubik2357x@gmail.com>
2022-08-29 19:58:23 +0900

    細かな変更

--- python/internal/_common_func.py
+++ python/internal/_common_func.py
@@ -4,5 +4,5 @@ import random
 import string
 
 
-def generate_random_string(nos, los) -> list[str]:
+def generate_random_string(nos: int, los: int) -> list[str]:
     return ["".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for j in range(los)) for i in range(nos)]

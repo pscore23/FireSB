45e4e777e1a6aed6d492d16d4f813a52d0c11d85
pscore23 <rubik2357x@gmail.com>
2022-08-29 18:50:10 +0900

    データ取得までの GUI 処理等を実装

--- python/runtime.py
+++ python/runtime.py
@@ -4,7 +4,10 @@ import os
 import sys
 
 import PySide6
-from PySide6.QtWidgets import QWidget, QPushButton, QTextEdit, QApplication
+from PySide6.QtWidgets import QWidget, QPushButton, QTextEdit, QLabel, QApplication
+
+from internal import _requester
+from internal.static.assets import style_sheets
 
 
 class MainWindow(QWidget):
@@ -14,30 +17,53 @@ class MainWindow(QWidget):
 
         super().__init__(parent)
 
-        self.button, self.text_edit = None, None
+        self.button, self.text_edit, self.label = None, None, None
 
         self.setWindowTitle("FireSB")
         self.resize(600, 500)
 
-        self.set_button()
-        self.set_text_edit()
+        self.set_label(150, 85)
+        self.set_button(320, 50)
+        self.set_text_edit(150, 50)
 
-    def set_button(self):
+    def set_label(self, x_pos, y_pos):
+        self.label = QLabel(self)
+
+        self.label.setStyleSheet(style_sheets.label_style)
+        self.label.setText("入力待ちです...")
+        self.label.move(x_pos, y_pos)
+        self.label.resize(250, 25)
+
+    def set_button(self, x_pos, y_pos):
         self.button = QPushButton(self)
 
+        self.button.setStyleSheet(style_sheets.button_style)
         self.button.setText("解析する!")
-        self.button.move(320, 50)
+        self.button.move(x_pos, y_pos)
         self.button.resize(75, 25)
-        self.button.clicked.connect(self.convert)
+        self.button.clicked.connect(self.analyze)
 
-    def set_text_edit(self):
+    def set_text_edit(self, x_pos, y_pos):
         self.text_edit = QTextEdit(self)
 
-        self.text_edit.move(150, 50)
+        self.text_edit.setStyleSheet(style_sheets.text_edit_style)
+        self.text_edit.move(x_pos, y_pos)
         self.text_edit.resize(150, 25)
 
-    def convert(self):
-        print(self.text_edit.toPlainText().translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)})))
+    def analyze(self):
+        self.button.setEnabled(False)
+
+        p_id = \
+            self.text_edit.toPlainText().translate(str.maketrans({chr(0xFF01 + x): chr(0x21 + x) for x in range(94)}))
+        data = _requester.Require.get_project_data(p_id)
+
+        if data is None:
+            self.label.setText("データの取得に失敗しました - 再試行してみてください")
+
+        else:
+            self.label.setText("データの取得が完了しました!")
+
+        print(data)
 
 
 app = QApplication(sys.argv)

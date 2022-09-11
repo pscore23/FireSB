79ea5e42f13fb6ceba8cdfb2c40edd573147501d
pscore23 <rubik2357x@gmail.com>
2022-09-11 14:22:47 +0900

    "argument_type" 関数を追加

--- python/internal/_common_func.py
+++ python/internal/_common_func.py
@@ -4,6 +4,20 @@ import random
 import string
 
 
+def argument_type(enum):
+    return {
+        1: "menu",
+        2: "block",
+        3: "note",
+        8: "direction",
+        9: "color",
+        10: "text",
+        11: "broadcast",
+        12: "variable",
+        13: "list"
+    }.get(enum, "number")
+
+
 def generate_random_string(nos: int, los: int) -> list[str]:
     return ["".join(random.SystemRandom().choice(
         string.ascii_letters + string.digits) for j in range(los)) for i in range(nos)]

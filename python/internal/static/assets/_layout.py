ff73108aab11f328e156eaf8e72d4bf99cce8f74
pscore23 <rubik2357x@gmail.com>
2022-09-15 19:07:51 +0900

    配列を初期化

--- python/internal/static/assets/_layout.py
+++ python/internal/static/assets/_layout.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 
-LABEL_STYLE = "QLabel { color: #000000 }"
-BUTTON_STYLE = "QPushButton { background: #99FFCC }"
-TEXT_EDIT_STYLE = "QTextEdit { background: #FFFFCC }"
+LAYOUT_STYLE_1 = []
